c923f672df9e1b44005b92c53ac6023fc7db119b
Mitzter <loranabbas@abv.bg>
2022-09-15 20:41:21 +0300

    Create negativeAndPositiveNumbers.js

---
+++ Arrays and Nested Arrays/negativeAndPositiveNumbers.js
@@ -0,0 +1,14 @@
+function solve(arr){
+    const result = [];
+
+    for(let num of arr){
+        if(num < 0) {result.unshift(num);}
+        else {result.push(num);}
+    }
+
+    for(let num of result){
+        console.log(num);
+    }
+}
+
+solve([7, -2, 8, 9]);
