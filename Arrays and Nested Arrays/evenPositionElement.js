3e7235f358658f0b9a10dd44bd7cee6f8ac185ad
Mitzter <loranabbas@abv.bg>
2022-09-15 20:26:38 +0300

    Create evenPositionElement.js

---
+++ Arrays and Nested Arrays/evenPositionElement.js
@@ -0,0 +1,11 @@
+function solve(input){
+    let result = ' ';
+
+    for (let i = 0; i < input.length; i+= 2){
+        result += input[i];
+        result += ' ';
+    }
+    console.log(result);
+}
+
+solve([20,30,40,50,60]);
