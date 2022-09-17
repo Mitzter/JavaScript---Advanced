7d5dcdff144ae57d84220929d030c0388040c0d2
Mitzter <loranabbas@abv.bg>
2022-09-17 21:39:42 +0300

    Create 7. sortingNumbers.js

---
+++ Arrays and Nested Arrays/Excercises/7. sortingNumbers.js
@@ -0,0 +1,20 @@
+//Write a function that sorts an array of numbers so that the first element is the smallest one, the second is the biggest one, the third is the second smallest one, the fourth is the second biggest one, and so on.
+
+//Return the resulting array.
+
+function sortNumbers(arr){
+    let sortedArr = [];
+    arr.sort((a,b) => a - b);
+    console.log(arr);
+    while(arr.length != 0){
+        let smallNumber = arr.shift();
+        let bigNumber = arr.pop();
+
+        sortedArr.push(smallNumber);
+        sortedArr.push(bigNumber);
+    }
+    console.log(sortedArr);
+    
+}
+
+sortNumbers([1, 65, 3, 52, 48, 63, 31, -3, 18, 56]);
