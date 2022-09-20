926b2a108c5250330ffb2510f04cbc2e6598a1a6
Mitzter <loranabbas@abv.bg>
2022-09-20 19:23:23 +0300

    Updated carFactory

--- Objects and Composition/Excercises/1. calorieObject.js
+++ Objects and Composition/Excercises/1. calorieObject.js
@@ -2,16 +2,18 @@ function calorieObject(data){
     let result = {}
 
     for(let i = 0; i < data.length; i+=2){
-        result[data[i]]  = data[i + 1];
+        result[data[i]]  = Number(data[i + 1]);
     }
 
-    console.log(res);
+    console.log(result);
 }
+
+
 
 calorieObject(['Yoghurt', '48', 'Rise', '138',
 
 'Apple', '52']);
 console.log("----------");
-calorieObject(['Potato', '93', 'Skyr', '63',
+calorieObject2(['Potato', '93', 'Skyr', '63',
 
 'Cucumber', '18', 'Milk', '42']);
---
+++ Objects and Composition/Excercises/3. carFactory.js
@@ -0,0 +1,66 @@
+function carFactory(data){
+    let result = {}
+    result.model = data.model;
+    
+    let engineCtor = {
+        "Small engine": {power: 90, volume: 1800},
+        "Normal engine": {power: 120, volume: 2400},
+        "Monster engine": {power: 200, volume: 3500}
+    }
+
+    if(data.power <= 90){
+        result.engine = engineCtor["Small engine"];
+    } else if (data.power <= 120){
+        result.engine = engineCtor["Normal engine"];
+    } else {
+        result.engine = engineCtor["Monster engine"];
+    }
+    
+    let carriageCtor = {
+        hatchback: function(color){
+            return{
+                type: "hatchback",
+                color
+            }
+        },
+        coupe: function(color){
+            return{
+                type: "coupe",
+                color
+            }
+        }
+    }
+    
+    result["carriage"] = carriageCtor[data.carriage](data.color);
+
+    let size;
+    if(data.wheelsize % 2 !== 0){
+        size = data.wheelsize
+    } else {
+        size = data.wheelsize - 1;
+    }
+    result.wheels = [size,size,size,size];
+    //console.table(result);
+    return result;
+    
+}
+
+carFactory({ model: 'VW Golf II',
+
+power: 90,
+
+color: 'blue',
+
+carriage: 'hatchback',
+
+wheelsize: 14 });
+
+carFactory({ model: 'Opel Vectra',
+
+power: 110,
+
+color: 'grey',
+
+carriage: 'coupe',
+
+wheelsize: 17 });
