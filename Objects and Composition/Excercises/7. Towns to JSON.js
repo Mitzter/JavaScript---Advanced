22f5fcfc0f1f96259513c6fb2ba01cf19d7b6fdf
Mitzter <loranabbas@abv.bg>
2022-09-24 19:33:05 +0300

    Update 7. Towns to JSON.js

--- Objects and Composition/Excercises/7. Towns to JSON.js
+++ Objects and Composition/Excercises/7. Towns to JSON.js
@@ -1,3 +1,11 @@
+//You're tasked to create and print a JSON from a text table. You will receive input as an array of strings, where each string represents a row of a table, with values on the row encompassed by pipes "|" and optionally spaces.
+//The table will consist of exactly 3 columns "Town", "Latitude" and "Longitude".
+//The Latitude and Longitude columns will always contain valid numbers. Check the examples to get a better understanding of your task.
+//The input comes as an array of strings – the first string contains the table’s headings, each next string is a row from the table.
+
+//The output should be an array of objects wrapped in JSON.stringify()
+//Latitude and Longitude must be parsed to numbers, and represented till the second digit after the decimal point!
+
 function townsToJSON(input){
     let arr = input.slice(0, 1);
     arr = arr[0].split(' | ');
