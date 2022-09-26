809f41a10b64d2aa84a90530f6e4d1d674b6a72a
Mitzter <loranabbas@abv.bg>
2022-09-26 20:28:14 +0300

    Update app.js

--- 05. DOM Manipulations and Events/03. Add-Delete/app.js
+++ 05. DOM Manipulations and Events/03. Add-Delete/app.js
@@ -1,3 +1,26 @@
 function addItem() {
-    //TODO...
+    let ul = document.getElementById('items');
+
+    let input = document.getElementById('newText')
+    let value = input.value + ' ';
+
+    let li = document.createElement('li');
+
+    let deleteButton = document.createElement('a');
+    
+    deleteButton.textContent = 'Delete';
+    deleteButton.style.color = 'red';
+
+    deleteButton.addEventListener('click', function(event){
+        event.target.parentElement.remove();
+    });
+
+    li.append(deleteButton);
+
+
+
+
+    ul.append(li);
+
+    input.value = '';
 }
