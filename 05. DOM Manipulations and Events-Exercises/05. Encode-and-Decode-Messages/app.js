406b13fad848c7b9407302e0e57deb6e0b48a319
Mitzter <loranabbas@abv.bg>
2022-09-27 20:47:57 +0300

    created Encode and Decode Messages

--- 05. DOM Manipulations and Events-Exercises/05. Encode-and-Decode-Messages/app.js
+++ 05. DOM Manipulations and Events-Exercises/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,33 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+    let buttons = document.querySelectorAll("button");
+    buttons[0].addEventListener("click", encode);
+    buttons[1].addEventListener("click", decode);
+
+    function encode(e){
+        let newMsg = "";
+        let currentText = e.target.parentElement.children[1].value;
+
+
+        for(let i = 0; i < currentText.length; i++){
+            let currentCh = currentText[i].charCodeAt();
+            newMsg += String.fromCharCode(currentCh + 1);
+        }
+        let textAreas = document.querySelectorAll("textarea");
+        let resultTextArea = textAreas[1];
+        resultTextArea.value = newMsg;
+        let currentTextArea = textAreas[0];
+        currentTextArea.value = "";
+    }
+
+    function decode(e){
+        let currentArea = e.target.parentElement.children[1];
+        let currentText = currentArea.value;
+        let resultMsg = "";
+        for(let i = 0; i < currentText.length; i++){
+            let ch = currentText[i].charCodeAt();
+            resultMsg += String.fromCharCode(ch - 1);
+        }
+
+        currentArea.value = resultMsg;
+    }
 }
