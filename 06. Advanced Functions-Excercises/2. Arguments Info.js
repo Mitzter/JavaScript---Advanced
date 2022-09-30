00b4e34004ca162cf4b2d22c3ad32e672be43a1e
Mitzter <loranabbas@abv.bg>
2022-09-30 19:38:25 +0300

    Update 2. Arguments Info.js

---
+++ 06. Advanced Functions-Excercises/2. Arguments Info.js
@@ -0,0 +1,19 @@
+function solve(...params){
+    let result = {};
+    for(let el of params){
+        let type = typeof(el)
+        console.log(`${type}: ${el}`)
+        if(!result.hasOwnProperty(type)){
+            result[type] = 0;
+        }
+    
+        result[type] = result[type] + 1;
+    }
+    
+    let buff = "";
+    for(let [k,v] of Object.entries(result)){
+        buff += `${k} = ${v}\n`
+    }
+
+    console.log(buff);
+}
