9a1edcc5e75f292eccf27f206e2201eb4070eda1
Mitzter <loranabbas@abv.bg>
2022-10-04 21:25:11 +0300

    created

--- 07. Unit Testing and Error Handling-Excercises/06.Dynamic-Validation/app.js
+++ 07. Unit Testing and Error Handling-Excercises/06.Dynamic-Validation/app.js
@@ -1,3 +1,15 @@
 function validate() {
-    // TODO
+    let input = document.getElementById("email");
+    input.addEventListener("change", onChange)
+
+    function onChange(e){
+        let email = e.target.value;
+        let emailPattern = /[a-z0_9]+@[a-z0_9]+.[a-z]+/g
+
+        if(emailPattern.test(email)){
+            e.target.classList.remove("error");
+        }else{
+            e.target.classList.add("error");
+        }
+    }
 }
