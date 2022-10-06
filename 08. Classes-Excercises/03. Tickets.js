ea8afe5578169540e3dc7cf0e5d1b17fa09eb37a
Mitzter <loranabbas@abv.bg>
2022-10-07 00:10:31 +0300

    Update 03. Tickets.js

--- 08. Classes-Excercises/03. Tickets.js
+++ 08. Classes-Excercises/03. Tickets.js
@@ -7,20 +7,39 @@ class Ticket{
 }
 
 function solve(arr, sortCriteria){
-    let arrSplit = arr.split(',');
+    
     let ticketStorage = [];
-    for(let i = 0; i < arrSplit.length; i++){
-        let currentTicket = arrSplit[i].split('|');
+    for(let i = 0; i < arr.length; i++){
+        let string = arr[i];
+        let currentTicket = string.split('|');
 
         let ticketDestination = currentTicket[0];
         let ticketPrice = currentTicket[1];
         let ticketStatus = currentTicket[2];
 
         let ticket = new Ticket(ticketDestination, ticketPrice, ticketStatus);
+        ticketStorage.push(ticket);
 
     }
     
+    switch(sortCriteria){
+        case "destination": ticketStorage.sort((a,b) => a.destination - b.destination);
+        break;
+        case "price": ticketStorage.sort((a,b => a.price > b.price ));
+        break;
+        case "status": ticketStorage.sort((a,b => a.status - b.status));
+        break;
+    }
     
-    
-    
+    return ticketStorage;
 }
+
+console.log(solve(['Philadelphia|94.20|available',
+
+'New York City|95.99|available',
+
+'New York City|95.99|sold',
+
+'Boston|126.20|departed'],
+
+'destination'))
