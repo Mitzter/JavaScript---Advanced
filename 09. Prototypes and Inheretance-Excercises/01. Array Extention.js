b5fc48f32ac2f2dd5012633a82f7b6a3212d3497
Mitzter <loranabbas@abv.bg>
2022-10-11 00:00:08 +0300

    created

---
+++ 09. Prototypes and Inheretance-Excercises/01. Array Extention.js
@@ -0,0 +1,33 @@
+Array.prototype.last = function (){
+    return this[this.length-1];
+}
+Array.prototype.skip = function(n){
+    let result = [];
+    for(i = n; i < this.length; i++){
+        result.push(this[i]);
+    }
+
+    return result;
+}
+
+Array.prototype.take = function(n){
+    let result = [];
+    for(i = 0; i < n; i++){
+        result.push(this[i]);
+    }
+
+    return result;
+}
+
+Array.prototype.sum = function(){
+    let result = 0;
+    for(i = 0; i < this.length; i++){
+        result+= this[i];
+    }
+
+    return result;
+}
+
+Array.prototype.average = function(){
+    return this.sum() / this.length;
+}
