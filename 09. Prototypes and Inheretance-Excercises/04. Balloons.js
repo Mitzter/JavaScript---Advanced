ff59451f7460fcccf6f883d6ada0e83683d59a54
Mitzter <loranabbas@abv.bg>
2022-10-11 20:01:21 +0300

    Create 04. Balloons.js

---
+++ 09. Prototypes and Inheretance-Excercises/04. Balloons.js
@@ -0,0 +1,60 @@
+ function solution(){
+
+
+    class Balloon {
+        constructor(color, hasWeight){
+            this.color = color;
+            this.hasWeight = hasWeight;
+        }
+    }
+
+    class PartyBalloon extends Balloon {
+        
+        constructor(color, hasWeight, ribbonColor, ribbonLength){
+            super(color, hasWeight);
+            this.ribbonColor = ribbonColor;
+            this.ribbonLength = ribbonLength;
+            this._ribbon = {
+                ribbonColor,
+                ribbonLength
+            }
+
+        }
+
+        get ribbon() {
+            return this._ribbon;
+        }
+    }
+
+    class BirthdayBallon extends PartyBalloon {
+        constructor(color,hasWeight,ribbonColor,ribbonLength,text){
+            super(color,hasWeight,ribbonColor,ribbonLength);
+            this.text = text;
+        }
+
+        get text(){
+            return this.text;
+        }
+    }
+
+    return {
+        Balloon,
+        PartyBalloon,
+        BirthdayBallon
+    }
+ }
+
+let classes = solution();
+
+let testBalloon = new classes.Balloon("yellow", 20.5);
+
+let testPartyBalloon = new classes.PartyBalloon("yellow", 20.5,"red", 10.25);
+
+let ribbon = testPartyBalloon.ribbon;
+
+console.log(testBalloon);
+
+console.log(testPartyBalloon);
+
+console.log(ribbon);
+
