9a3a94b028b17c6d9405492fbf96be9b7310474b
Mitzter <loranabbas@abv.bg>
2022-10-20 12:05:36 +0300

    aa

--- 05. DOM Manipulations and Events-Exercises/03. Locked-Profile/app.js
+++ 05. DOM Manipulations and Events-Exercises/03. Locked-Profile/app.js
@@ -1,11 +1,25 @@
 function lockedProfile() {
-    let profiles = document.getElementsByTagName('div');
-    let button = document.getElementsByClassName('button')
-    for(let i = 0; i < profiles.length; i++){
-        
+    const btns = [...document.getElementsByTagName('button')];
+    btns.forEach(btn => btn.addEventListener('click',showHide));
+
+    function showHide(event){
+        const button = event.target;
+        const profile = button.parentNode;
+        const moreInformation = profile.getElementsByTagName('div')[0];
+        const lockStatus = profile.querySelector('input[type="radio"]:checked').value;
+
+        if(lockStatus === 'unlock'){
+            if(button.textContent === 'Show more'){
+                moreInformation.style.display = 'inline-block';
+                button.textContent = 'Hide it';
+            } else if(button.textContent === 'Hide it'){
+                moreInformation.style.display = 'none';
+                button.textContent = 'Show more';
+            }
+        }
+
+        debugger
     }
-
-
 
     
 }
--- 05. DOM Manipulations and Events-Exercises/04. Fill-Dropdown/dropdown.js
+++ 05. DOM Manipulations and Events-Exercises/04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,27 @@
 function addItem() {
-    console.log('TODO:...');
+    const text = document.getElementById("newItemText");
+    const value = document.getElementById("newItemValue");
+    const button = document.querySelector("input[type=button]");
+    const menu = document.getElementById("menu");
+
+    
+    
+    
+       
+        let textValue = text.value;
+        let valueValue = value.value;
+        if(!textValue || !valueValue){
+            return;
+        }
+        let option = document.createElement("option");
+        option.textContent = textValue;
+        option.value = valueValue;
+
+        menu.appendChild(option);
+
+        text.value = "";
+        value.value = "";
+
+        
+    
 }
