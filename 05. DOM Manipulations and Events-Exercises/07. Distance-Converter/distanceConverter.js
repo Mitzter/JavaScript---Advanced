232a9c07e2cc21ee66b5c573029404f93617b2e8
Mitzter <loranabbas@abv.bg>
2022-10-20 18:16:48 +0300

    Update distanceConverter.js

--- 05. DOM Manipulations and Events-Exercises/07. Distance-Converter/distanceConverter.js
+++ 05. DOM Manipulations and Events-Exercises/07. Distance-Converter/distanceConverter.js
@@ -1,3 +1,33 @@
 function attachEventsListeners() {
-    console.log('TODO:...');
+    const input = document.getElementById("inputDistance");
+    const inputUnits = document.getElementById("inputUnits");
+    const output = document.getElementById("outputDistance");
+    const outputUnits = document.getElementById("outputUnits");
+
+
+    const convertBtn = document.getElementById("convert").addEventListener("click", convert);
+
+    const convertEnumerator= {
+        km: 1000,
+        m: 1,
+        cm: 0.01,
+        mm: 0.001,
+        mi: 1609.34,
+        yrd: 0.9144,
+        yrd: 0.9144,
+        ft: 0.3048,
+        in: 0.0254
+    }
+
+    function convert(ev){
+        let inputValue = input.value;
+        let inputUnitsValue = inputUnits.value;
+        let outputUnitsValue = outputUnits.value;
+        
+        let inputValueInMeters = inputValue * convertEnumerator[inputUnitsValue];
+        output.value = inputValueInMeters / convertEnumerator[outputUnitsValue];
+        
+        
+    }
+
 }
