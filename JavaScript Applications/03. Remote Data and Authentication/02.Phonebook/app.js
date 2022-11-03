e155c52bdef9ef15890c115e1ca1edbcbc34f5cb
Mitzter <loranabbas@abv.bg>
2022-11-03 20:46:35 +0200

    a

--- JavaScript Applications/03. Remote Data and Authentication/02.Phonebook/app.js
+++ JavaScript Applications/03. Remote Data and Authentication/02.Phonebook/app.js
@@ -1,5 +1,83 @@
 function attachEvents() {
-    console.log('TODO...');
+    document.getElementById("btnLoad").addEventListener("click", onLoadAllRecords);
+    document.getElementById("btnCreate").addEventListener("click", handleCreateRecord);
 }
 
+function handleCreateRecord(){
+    const personEl = document.getElementById("person");
+    const phoneEl = document.getElementById("phone");
+
+    onCreateRecord(personEl.value, phoneEl.value);
+    personEl.value = "";
+    phoneEl.value = "";
+
+
+}
+
+function renderRecord(data){
+    debugger
+    const ul = document.getElementById("phonebook");
+    ul.innerHTML = "";
+    Object.values(data).forEach(rec => {
+        const li = document.createElement("li");
+        li.textContent = `${rec.person}: ${rec.phone}`
+        li.setAttribute("data-id", rec._id);
+
+        const btn = document.createElement("button");
+        btn.textContent = "Delete";
+        btn.addEventListener("click", handleDelete)
+        li.appendChild(btn);
+        ul.appendChild(li);
+    })
+    
+}
+
+
+function handleDelete(e){
+    const li = e.target.parentElement;
+    const id = li.getAttribute("data-id");
+
+    onDeleteRecord(id);
+    li.remove();
+}
+
+async function onLoadAllRecords(){
+    const url = "http://localhost:3030/jsonstore/phonebook";
+    const response = await fetch(url);
+    const data = await response.json();
+
+    return renderRecord(data);
+}
+
+async function onCreateRecord(person, phone) {
+    const url = "http://localhost:3030/jsonstore/phonebook";
+    const body = {
+        person,
+        phone
+    }
+    const header = getHeader("POST", body)
+    const response = await fetch(url, header);
+
+    const data = await response.json();
+    onLoadAllRecords();
+    return data;
+}
+
+async function onDeleteRecord(id){
+    const url = `http://localHost:3030/jsonstore/phonebook/${id}`;
+    
+    const headers = getHeader("DELETE", null)
+    
+
+}
+
+function getHeader(method, body){
+    return {
+        method: `${method}`,
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(body)
+    }
+}
 attachEvents();
