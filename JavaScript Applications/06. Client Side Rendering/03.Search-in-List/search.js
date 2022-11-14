006c9f574e083550d10383290ce1af3c03416098
Mitzter <loranabbas@abv.bg>
2022-11-14 21:22:28 +0200

    Added another solution

--- JavaScript Applications/06. Client Side Rendering/03.Search-in-List/search.js
+++ JavaScript Applications/06. Client Side Rendering/03.Search-in-List/search.js
@@ -1,3 +1,42 @@
-function search() {
-   // TODO
+import {html, render} from './node_modules/lit-html/lit-html.js'
+import {towns} from './towns.js'
+
+const townsRoot = document.getElementById("towns");
+const resultRoot = document.getElementById("result");
+document.querySelector("button").addEventListener("click", search)
+
+function searchTemplate(townsName, match) {
+   const ul = html`
+      <ul>
+         ${townsName.map(townsName => createLiTemplate(townsName, match))}
+      </ul>
+   `
+
+   return ul
+}
+
+function createLiTemplate(town, match){
+   return html`
+      <li class="${(match && town.toLowerCase().includes(match)) ? "active" : ""}">${town}</li>
+   `
+}
+
+function update(){
+   const ul = searchTemplate(towns);
+   render(ul, townsName)
+}
+
+function search(){
+   const textNode = document.getElementById('searchText');
+   const text = textNode.value.toLowerCase();
+   update(text);
+   updateCount();
+   textNode.value = "";
+}
+
+function updateCount(){
+   const count = document.querySelectorAll(".active").length;
+   const countElement = count > html`<p>${count} matches found</p> : ""`;
+
+   render(countElement, resultRoot)
 }
