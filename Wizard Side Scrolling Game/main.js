fad38119139d76e35afb689d28a6d94e3efe4cdd
Mitzter <loranabbas@abv.bg>
2022-11-14 20:54:25 +0200

    Created Client Side Rendering Solutions

--- Wizard Side Scrolling Game/main.js
+++ Wizard Side Scrolling Game/main.js
@@ -11,13 +11,13 @@ const fireballSound = new Audio('./sounds/brrshh.mp3');
 const flyingSound = document.createElement('audio');
 flyingSound.setAttribute('src', './sounds/flyingSound.mp3');
 flyingSound.setAttribute('autoplay', 'autoplay');
-flyingSound.playbackRate=2.0;
+flyingSound.playbackRate = 2.0;
 flyingSound.volume = 1;
 
 
 gameStart.addEventListener('click', onGameStart);
 
-function onGameStart(){
+function onGameStart() {
     gameStart.classList.add('hide');
     audioObj.play();
     window.requestAnimationFrame(gameAction);
@@ -44,13 +44,13 @@ let player = {
 };
 let game = {
     speed: 2,
-    movingMultiplier: 4,
-    fireBallMultiplier: 5,
+    movingMultiplier: 5,
+    fireBallMultiplier: 6,
     fireInterval: 500,
     cloudSpawnInterval: 3000,
-    bugSpawnInterval: 1000,
+    bugSpawnInterval: 400,
     bugKillBonus: 2000
-    
+
 };
 let scene = {
     score: 0,
@@ -62,46 +62,46 @@ let scene = {
 document.addEventListener('keydown', onKeyDown);
 document.addEventListener('keyup', onKeyUp);
 
-function onKeyDown(e){
+function onKeyDown(e) {
     keys[e.code] = true;
     console.log(keys);
 }
-function onKeyUp(e){
+function onKeyUp(e) {
     keys[e.code] = false;
     console.log(keys);
 }
 
-function gameAction(timestamp){
+function gameAction(timestamp) {
     const wizard = document.querySelector('.wizard');
 
     let isInAir = (player.y + player.height) <= gameArea.offsetHeight
-    if(isInAir) {
+    if (isInAir) {
         player.y += game.speed;
     }
 
-    if(keys.ArrowUp && player.y > 0){
+    if (keys.ArrowUp && player.y > 0) {
         player.y -= game.speed * game.movingMultiplier;
         flyingSound.play();
     }
-    if(keys.ArrowDown && isInAir){
+    if (keys.ArrowDown && isInAir) {
         player.y += game.speed * game.movingMultiplier;
         flyingSound.play();
     }
-    if(keys.ArrowLeft && player.x > 0){
+    if (keys.ArrowLeft && player.x > 0) {
         player.x -= game.speed * game.movingMultiplier;
         flyingSound.play();
     }
-    if(keys.ArrowRight && player.x + player.width < gameArea.offsetWidth){   
+    if (keys.ArrowRight && player.x + player.width < gameArea.offsetWidth) {
         player.x += game.speed * game.movingMultiplier;
         flyingSound.play();
     }
-    if(keys.Space && timestamp - player.lastTimeFiredFireball > game.fireInterval){
+    if (keys.Space && timestamp - player.lastTimeFiredFireball > game.fireInterval) {
         wizard.classList.add('wizard-fire');
         addFireBall(player);
-        
+
         player.lastTimeFiredFireball = timestamp;
     } else {
-        
+
         wizard.classList.remove('wizard-fire');
     }
 
@@ -116,12 +116,13 @@ function gameAction(timestamp){
         fireBall.x += game.speed * game.fireBallMultiplier;
         fireBall.style.left = fireBall.x + 'px';
 
-        if(fireBall.x + fireBall.offsetWidth > gameArea.offsetWidth){
+
+        if (fireBall.x + fireBall.offsetWidth > gameArea.offsetWidth) {
             fireBall.parentElement.removeChild(fireBall);
         }
     });
 
-    if(timestamp - scene.lastCloudSpawn > game.cloudSpawnInterval + 20000 * Math.random()){
+    if (timestamp - scene.lastCloudSpawn > game.cloudSpawnInterval + 20000 * Math.random()) {
         let cloud = document.createElement('div');
         cloud.classList.add('cloud');
         cloud.x = gameArea.offsetWidth - 200;
@@ -133,37 +134,37 @@ function gameAction(timestamp){
     }
 
     let clouds = document.querySelectorAll('.cloud');
-    
+
     clouds.forEach(cloud => {
         cloud.x -= game.speed;
         cloud.style.left = cloud.x + 'px';
 
-        if(cloud.x + clouds.offsetWidth <= 0){
+        if (cloud.x + clouds.offsetWidth <= 0) {
             cloud.parentElement.removeChild(cloud);
         }
     })
 
-    if(timestamp - scene.lastBugSpawn > game.bugSpawnInterval + 5000 * Math.random()){
+    if (timestamp - scene.lastBugSpawn > game.bugSpawnInterval + 5000 * Math.random()) {
         let bug = document.createElement('div');
         bug.classList.add('bug');
         bug.x = gameArea.offsetWidth - 60;
         bug.style.left = bug.x + 'px';
         bug.style.top = (gameArea.offsetHeight - 60) * Math.random() + 'px';
-        
+
         gameArea.appendChild(bug);
         scene.lastBugSpawn = timestamp;
-     }
+    }
 
-    
+
 
     let bugs = document.querySelectorAll('.bug');
     bugs.forEach(bug => {
-        if(isCollision(wizard, bug)) {
+        if (isCollision(wizard, bug)) {
             gameOverAction();
         }
 
-        fireBalls.forEach(fireBall =>{
-            if(isCollision(fireBall, bug)){
+        fireBalls.forEach(fireBall => {
+            if (isCollision(fireBall, bug)) {
                 scene.score += game.bugKillBonus;
                 bug.parentElement.removeChild(bug);
                 bugDeathSound.cloneNode(true).play();
@@ -172,48 +173,42 @@ function gameAction(timestamp){
         })
         bug.x -= game.speed * 3;
         bug.style.left = bug.x + 'px';
-        if(bug.x + bugs.offsetWidth <= 0){
+        if (bug.x + bugs.offsetWidth <= 0) {
             bug.parentElement.removeChild(bug);
         }
     });
-    
-    if(scene.isActiveGame){
-        window.requestAnimationFrame(gameAction);   
+
+    if (scene.isActiveGame) {
+        window.requestAnimationFrame(gameAction);
     }
-    
-
-function addFireBall() {
-    let fireBall = document.createElement('div');
-    fireballSound.cloneNode(true).play();
-    fireBall.classList.add('fire-ball');
-    fireBall.style.top = (player.y + player.height / 3 - 5) + 'px';
-    fireBall.x = player.x + player.width;
-    fireBall.style.left = fireBall.x + 'px';
-
-    gameArea.appendChild(fireBall);
-}
-
-function isCollision(firstElement, secondElement){
-    let firstRect = firstElement.getBoundingClientRect();
-    let secondRect = secondElement.getBoundingClientRect();
-    
-    return !(firstRect.top > secondRect.bottom ||
-        firstRect.bottom < secondRect.top ||
-        firstRect.right < secondRect.left ||
-        firstRect.left > secondRect.right)
-}
-
-function gameOverAction() {
-    scene.isActiveGame = false;
-    gameOver.classList.remove('hide');
-}
 
 
+    function addFireBall() {
+        let fireBall = document.createElement('div');
+        fireballSound.cloneNode(true).play();
+        fireBall.classList.add('fire-ball');
+        fireBall.style.top = (player.y + player.height / 3 - 5) + 'px';
+        fireBall.x = player.x + player.width;
+        fireBall.style.left = fireBall.x + 'px';
+
+        gameArea.appendChild(fireBall);
+    }
 
 
+    function isCollision(firstElement, secondElement) {
+        let firstRect = firstElement.getBoundingClientRect();
+        let secondRect = secondElement.getBoundingClientRect();
 
+        return !(firstRect.top > secondRect.bottom ||
+            firstRect.bottom < secondRect.top ||
+            firstRect.right < secondRect.left ||
+            firstRect.left > secondRect.right)
+    }
 
-
+    function gameOverAction() {
+        scene.isActiveGame = false;
+        gameOver.classList.remove('hide');
+    }
 
 
 
